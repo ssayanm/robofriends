065cd8c420aa4e2acbc4bb4df65e02758ba3ddfa
Sayan Mukherjee <hello@sayanmukherjee.com>
2019-10-21 10:15:57 +0530

    Updates

---
+++ precache-manifest.7e47917a12a9df5fc13652a8ec1ac06b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f9b4f67f90be2a2e4393273ecbdf37",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ec4c7d95ddb0a8193626",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b95b5577bb090072ee7b",
+    "url": "/robofriends/static/css/main.31163190.chunk.css"
+  },
+  {
+    "revision": "ec4c7d95ddb0a8193626",
+    "url": "/robofriends/static/js/2.a0a88723.chunk.js"
+  },
+  {
+    "revision": "b95b5577bb090072ee7b",
+    "url": "/robofriends/static/js/main.da01ae36.chunk.js"
+  },
+  {
+    "revision": "1eafa76e58301bf2aef6",
+    "url": "/robofriends/static/js/runtime~main.b45798a7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
