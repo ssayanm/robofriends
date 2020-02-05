7d814b3959576ceaa2af485d59d61db6c9c4b1c0
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-02-05 10:15:08 +0530

    added enzyme for testing

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const Card = (props) => {
-	const { name, email, id } = props;
-	return (
-	    <div className="bg-light-blue tc dib br3 pa3 ma2 grow bw2 shadow-5">
-	    	<img src={`https://robohash.org/${id}?200x200`} alt='robo'/>
-	      	<div>
-	      		<h2>{name}</h2>
-	      		<p>{email}</p>
-	      	</div>
-	    </div>
+const Card = props => {
+  const { name, email, id } = props;
+  return (
+    <div className="bg-light-blue tc dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img src={`https://robohash.org/${id}?200x200`} alt="robo" />
+      <div>
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    </div>
   );
-}
+};
 
 export default Card;
